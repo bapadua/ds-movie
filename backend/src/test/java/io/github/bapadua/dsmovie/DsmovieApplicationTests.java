8d8fd4071342729662da526d5e4693a288edba92
Bruno A. Padua <bapadua@stefanini.com>
2022-01-14 16:07:12 -0300

    Project created

---
+++ backend/src/test/java/io/github/bapadua/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bapadua.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
