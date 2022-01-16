6e04a54a7ec2a32bdafc0da3bd73dd46f1d800a0
Bruno A. Padua <bapadua@stefanini.com>
2022-01-16 16:55:54 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -28,21 +28,22 @@ function Listing() {
                 const data = response.data as MoviePage;
                 setPage(data)
             })
-    }, [pageNumber])
+    }, [pageNumber]);
 
-
+    const handlePageChange = (newPageNumber: number) => {
+        setPageNumber(newPageNumber);
+    }
 
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
             <div className='container'>
                 <div className="row">
                     {page.content.map(movie => (
-                        <div key={movie.id}className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                             <MovieCard movie={movie} />
                         </div>
-                    )
-                    )}
+                    ) )}
                 </div>
             </div>
         </>
